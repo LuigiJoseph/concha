33fbac741aa3b6a2e80dc6e667708a3a35cfddda
Mohammed Alyousuf <luigiornojoseph@gmail.com>
2024-06-21 10:04:15 +0100

    launch function similar to execute function in main program

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+
+
 int main(int argc, char **argv)
 {
   // Load config files, if any.
@@ -29,6 +33,7 @@ void lsh_loop(void){
     } while (status);
 }
 
+//reading line
 #define LSH_RL_BUFSIZE 1024
 char *lsh_read_line(void){
     int bufsize = LSH_RL_BUFSIZE;
@@ -55,7 +60,7 @@ char *lsh_read_line(void){
     }
 
     //If we have exceeded the buffer, rellocate memory size
-    if (position >= buffsize){
+    if (position >= bufsize){
         bufsize += LSH_RL_BUFSIZE;
         buffer = realloc(buffer, bufsize);
         if (!buffer) {
@@ -64,3 +69,64 @@ char *lsh_read_line(void){
         }
     }
 }
+
+//parsing line
+#define LSH_TOK_BUFSIZE 64;
+#define LSH_TOK_DELIM " \t\r\n\a";
+char **lsh_split_line(char *line){
+    int bufsize = LSH_TOK_BUFSIZE
+    int position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens){
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    token = strtok(line, LSH_TOK_DELIM);
+    while (token != NULL){
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize){
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens){
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+
+        }
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+
+}
+
+int lsh_launch(char **args){
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0){
+        //child process
+        if (execvp(args[0], args) == -1){
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if(pid < 0){
+        //error forking
+        perror("lsh");
+    }
+    else{
+        //parent process
+        do{
+            wpid = waitpid(pid, &status, WUNTRACED);
+        }
+        while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
+
