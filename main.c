29d3d5c1f80052677a8b8c71595b85b436092418
Mohammed Alyousuf <luigiornojoseph@gmail.com>
2024-06-21 10:09:33 +0100

    Declartions of builtin shell comands

--- main.c
+++ main.c
@@ -130,3 +130,31 @@ int lsh_launch(char **args){
     return 1;
 }
 
+/*
+    function declartions for builtin shell commands
+*/
+
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+/*
+    List of built-in commands followed by their corresponding functions
+*/
+
+char *builtin_str[] {
+    "cd",
+    "help",
+    "exist"
+};
+
+int (*builtin_func[]) (char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit
+};
+
+int lsh_num_builtins(){
+    return sizeof(builtin_str) / sizeof(char*);
+}
+
